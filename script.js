46d7976858c5d56ee51ffda71077d1a74b9d8117
zaddyofph <106165879+zaddyofph@users.noreply.github.com>
2022-06-25 18:35:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-var fname = window.prompt("Enter your name: ");
-alert(fname);
-var height = window.prompt("Enter your height :", "0 Ft");
-alert(height);
-var country = window.prompt("Enter your country: ");
-alert(country);
+let chooseOperator = window.prompt("choose an operator: ");
+let first = window.prompt('enter a first number: ');
+let second = window.prompt('enter a second number: ');
+
+if (chooseOperator === "+"){
+    let add = parseInt(first) + parseInt(second);
+    console.log(add);
+    alert(add);
+}
+
+else if (chooseOperator === "-"){
+    let subtract = parseInt(first) - parseInt(second);
+    alert(subtract);
+}
+else if (chooseOperator === "*"){
+    let multiply = parseInt(first) * parseInt(second);
+    alert(multiply);
+}
+else if (chooseOperator === "/"){
+    let divide = parseInt(first) / parseInt(second);
+    alert(divide);
+}
+else{
+    chooseOperator;
+}
