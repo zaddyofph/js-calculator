d7d640288070866e9148fc2ec45a5adf521858cb
zaddyofph <106165879+zaddyofph@users.noreply.github.com>
2022-06-25 18:33:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var fname = window.prompt("Enter your name: ");
+alert(fname);
+var height = window.prompt("Enter your height :", "0 Ft");
+alert(height);
+var country = window.prompt("Enter your country: ");
+alert(country);
